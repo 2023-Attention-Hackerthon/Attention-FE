7abb9a3746bca10d0155ac25ffe7675178126406
bomin <nimod1234@g.skku.edu>
2023-11-26 05:17:58 +0900

    지갑 상세 페이지 생성 (#28)

--- screens/wallet/MyWalletsScreen.tsx
+++ screens/wallet/MyWalletsScreen.tsx
@@ -1,94 +1,95 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { View, TouchableOpacity, Image, ImageSourcePropType, SafeAreaView, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Image, ImageSourcePropType, SafeAreaView } from "react-native";
 import Typography from "../../components/common/Typography";
 import RoutePath from "../../navigation/routePath";
 import Colors from "../../constants/Colors";
-import baseAxios from "../../apis/baseAxios";
-
-interface WalletProps {
-  createdDate: string;
-  id: number;
-  imageUrls: any[];
-  modifiedDate: string;
-  name: string;
-  status: string;
-  user: {
-    authInfo: { accountStatus: string; email: string; loginType: string; role: string };
-    createdDate: string;
-    id: number;
-    modifiedDate: string;
-    name: null;
-    universityName: null;
-  };
-}
+import { baseAxios } from "../../apis/baseAxios";
+import { useQuery } from "@tanstack/react-query";
+import { WalletList } from "../../types/Wallet";
+import { ErrorText, LoadingText } from "./MyWalletDetailScreen";
 
 export default function MyWalletsScreen() {
   const navigation = useNavigation();
 
-  const [wallets, setWallets] = useState<WalletProps[]>([]);
+  const [wallets, setWallets] = useState<WalletList>([]);
 
-  const navigateDetailWalletPage = (wallet: number) => {
+  const navigateDetailWalletPage = (id: number) => {
     //@ts-ignore
     navigation.navigate(RoutePath.MyWalletDetailScreen, {
       params: {
-        wallet,
+        id,
       },
     });
   };
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["getWalletList"],
+    queryFn: getWalletList,
+  });
 
   useEffect(() => {
-    const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
-    baseAxios
-      .get("/api/wallets", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((res) => {
-        setWallets(res.data.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    if (data) {
+      setWallets(data.data);
+    }
+  }, [data]);
+
+  if (isError) {
+    return <ErrorText />;
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#ffffff", justifyContent: "space-between" }}>
-      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }}>
-        {wallets.map((wallet: WalletProps, idx: number) => {
-          let url: ImageSourcePropType | undefined;
-          switch (idx % 3) {
-            case 0:
-              url = require("../../assets/images/wallet-1.png");
-              break;
-            case 1:
-              url = require("../../assets/images/wallet-2.png");
-              break;
-            case 2:
-              url = require("../../assets/images/wallet-3.png");
-              break;
-            default:
-              url = require("../../assets/images/wallet-1.png");
-              break;
-          }
-          return (
-            <TouchableOpacity style={{ alignItems: "center", marginBottom: 20 }} onPress={() => navigateDetailWalletPage(wallet.id)} key={wallet.id}>
-              <Image source={url} style={{ width: 80, height: 112, marginBottom: 8 }} />
-              <Typography>{wallet.name}</Typography>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-      <TouchableOpacity style={{ borderWidth: 1, borderRadius: 99, marginBottom: 60, marginHorizontal: 15, backgroundColor: Colors.primary.normal }}>
-        <Typography style={{ textAlign: "center", paddingVertical: 8 }}>지갑 만들기</Typography>
-      </TouchableOpacity>
+      {isLoading || !wallets ? (
+        <LoadingText />
+      ) : (
+        <>
+          <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }}>
+            {wallets.map((wallet, idx: number) => {
+              let url: ImageSourcePropType | undefined;
+              switch (idx % 3) {
+                case 0:
+                  url = require("../../assets/images/wallet-1.png");
+                  break;
+                case 1:
+                  url = require("../../assets/images/wallet-2.png");
+                  break;
+                case 2:
+                  url = require("../../assets/images/wallet-3.png");
+                  break;
+                default:
+                  url = require("../../assets/images/wallet-1.png");
+                  break;
+              }
+              return (
+                <TouchableOpacity
+                  style={{ alignItems: "center", marginBottom: 20 }}
+                  onPress={() => navigateDetailWalletPage(wallet.id)}
+                  key={wallet.id}
+                >
+                  <Image source={url} style={{ width: 80, height: 112, marginBottom: 8 }} />
+                  <Typography>{wallet.name}</Typography>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+          <TouchableOpacity
+            style={{
+              borderWidth: 1,
+              borderRadius: 99,
+              marginBottom: 60,
+              marginHorizontal: 15,
+              backgroundColor: Colors.primary.normal,
+            }}
+          >
+            <Typography style={{ textAlign: "center", paddingVertical: 8 }}>지갑 만들기</Typography>
+          </TouchableOpacity>
+        </>
+      )}
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    justifyContent: "space-between",
-  },
-});
+export const getWalletList = async () => {
+  const { data } = await baseAxios.get(`/api/wallets`);
+  return data;
+};
--- types/Wallet.ts
+++ types/Wallet.ts
@@ -1,2 +1,17 @@
-export type Wallet = { id: number };
+export type Wallet = {
+  createdDate: string;
+  id: number;
+  imageUrls: any[];
+  modifiedDate: string;
+  name: string;
+  status: string;
+  user: {
+    authInfo: { accountStatus: string; email: string; loginType: string; role: string };
+    createdDate: string;
+    id: number;
+    modifiedDate: string;
+    name: null;
+    universityName: null;
+  };
+};
 export type WalletList = Wallet[];
