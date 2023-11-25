4a5800e4949d5bf6c9a9e3546879167106ef7ad6
nimod7890 <nimod1234@g.skku.edu>
2023-11-26 05:17:21 +0900

    지갑 상세 페이지 생성

--- apis/baseAxios.ts
+++ apis/baseAxios.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
 
-const baseAxios = axios.create({
+export const baseAxios = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default baseAxios;
+baseAxios.interceptors.request.use((config) => {
+  if (!config.headers) {
+    throw new Error(`Expected 'config' and 'config.headers' not to be undefined`);
+  }
+
+  const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
