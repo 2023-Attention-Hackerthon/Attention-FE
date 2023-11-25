306a2e67a8219052a44c59b5b40e3ebd1e7e208a
gs0428 <lider28@naver.com>
2023-11-26 04:29:55 +0900

    feat: 성별 컴포넌트 생성

--- screens/card/CreateCardScreen.tsx
+++ screens/card/CreateCardScreen.tsx
@@ -3,7 +3,7 @@ import { Card, Gender } from "../../types/Card";
 import InputFormContainer from "../../components/common/InputFormContainer";
 import UnderlinedInput from "../../components/common/UnderlinedInput";
 // import UnderlinedSelectForm from "../../components/common/UnderlinedSelectForm";
-import { ScrollView, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, TextInput, TouchableOpacity, View } from "react-native";
 import Typography from "../../components/common/Typography";
 import Colors from "../../constants/Colors";
 
@@ -14,6 +14,14 @@ export default function CreateCardScreen() {
     cardname: "",
     contact: "",
   });
+
+  const genders: string[] = ["남자", "여자", "선택 안함"];
+
+  const [isListOpen, setIsListOpen] = useState<boolean>(false);
+  const [genderQuestion, setGenderQuestion] = useState<string>("당신의 성별을 알려주세요.");
+  // 0 : 남자, 1 : 여자, 2 : 선택 안함
+  const [selectedGender, setSelectedGender] = useState<number>(0);
+  const listOpenImage = isListOpen ? require("../../assets/images/up.png") : require("../../assets/images/non-activated-down.png");
 
   // const { mutate: createCard, isLoading } = useMutation({
   //   // 여기에 axios function
@@ -43,13 +51,34 @@ export default function CreateCardScreen() {
 
   // Todo: isLoading 일때 카드 로딩 페이지
 
+  const SelectGender = (props: { gender: string; idx: number }) => {
+    const activatedValue =
+      props.idx === selectedGender ? require("../../assets/images/activated-check.png") : require("../../assets/images/non-activated-check.png");
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 8, paddingBottom: 8, paddingTop: 16 }}
+        onPress={() => {
+          setSelectedGender(props.idx);
+          setIsListOpen(!isListOpen);
+          if (props.idx === 0) {
+            setGenderQuestion("남자");
+          } else if (props.idx === 1) {
+            setGenderQuestion("여자");
+          } else {
+            setGenderQuestion("선택 안함");
+          }
+        }}
+      >
+        <Typography style={{ color: props.idx === selectedGender ? Colors.primary.normal : Colors.text.alternative }}>{props.gender}</Typography>
+        <Image source={activatedValue} />
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
-      <InputFormContainer
-        question={`다른 사람들이 \n당신을 뭐라고\n 부르면 좋을까요?`}
-        formNumber={1}
-        helperText="최대 5자 이하로 적어주세요"
-      >
+      <InputFormContainer question={`다른 사람들이 \n당신을 뭐라고\n 부르면 좋을까요?`} formNumber={1} helperText="최대 5자 이하로 적어주세요">
         <UnderlinedInput
           maxLength={5}
           value={(card?.nickname ?? "").toString()}
@@ -57,7 +86,31 @@ export default function CreateCardScreen() {
           placeholder="이름이나 별명을 적어주세요"
         />
       </InputFormContainer>
-      <InputFormContainer question={`당신의 성별을 \n알려주세요.`} formNumber={2}>
+      <InputFormContainer question={"당신의 성별을 \n알려주세요."} formNumber={2}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{ flexDirection: "row", justifyContent: "space-between", borderBottomWidth: 1, padding: 8 }}
+          onPress={() => {
+            if (isListOpen) {
+              if (selectedGender === 0) {
+                setGenderQuestion("남자");
+              } else if (selectedGender === 1) {
+                setGenderQuestion("여자");
+              } else {
+                setGenderQuestion("선택 안함");
+              }
+            }
+            setIsListOpen(!isListOpen);
+          }}
+        >
+          <Typography style={{ color: Colors.text.alternative }}>{genderQuestion}</Typography>
+          <Image source={listOpenImage} />
+        </TouchableOpacity>
+        <View style={{ display: isListOpen ? "flex" : "none" }}>
+          {genders.map((gender: string, idx: number) => (
+            <SelectGender gender={gender} idx={idx} key={gender + idx} />
+          ))}
+        </View>
         {/* todo */}
         {/* <UnderlinedSelectForm
           options={["남자", "여자", "선택 안 함"]}
@@ -113,21 +166,4 @@ export default function CreateCardScreen() {
   );
 }
 
-const MBTI = [
-  "ISTJ",
-  "ISFJ",
-  "INFJ",
-  "INTJ",
-  "ISTP",
-  "ISFP",
-  "INFP",
-  "INTP",
-  "ESTP",
-  "ESFP",
-  "ENFP",
-  "ENTP",
-  "ESTJ",
-  "ESFJ",
-  "ENFJ",
-  "ENTJ",
-];
+const MBTI = ["ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP", "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ"];
